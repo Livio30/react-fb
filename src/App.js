93ab1a4f85e2f56730809c42699c0d4b9cbd49f4
Livio Fran Mascarenhas <liviofranm@gmail.com>
2024-01-06 23:26:34 +0530

    hi

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import "./App.css";
 import { Auth } from "./components/auth";
 import { auth, db, storage } from "./config/firebase";
